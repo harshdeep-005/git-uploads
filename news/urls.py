44a39d46ce4b23c5614c84ff3884710892343ad0
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2025-06-10 19:24:32 +0530

    Delete news directory

--- news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from news.views import scrape, news_list
-
-urlpatterns = [
-    path('scrape/<str:name>/', scrape, name="scrape"),  # <-- Added trailing slash (recommended)
-    path('', news_list, name="home"),
-]
-
-
